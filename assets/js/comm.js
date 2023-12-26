77c060c5566c5f0448fb77b9cca68d2c1f2dfa7a
yejjjin <ggreat7712@gmail.com>
2023-12-26 17:40:14 +0900

    231226 work

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,26 +1,29 @@
-new Swiper(".swiper-container", {
-  direction: "vertical", // 수직 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
-});
+// new Swiper(".swiper-container", {
+//   direction: "vertical", // 수직 슬라이드
+//   autoplay: true, // 자동 재생 여부
+//   loop: true, // 반복 재생 여부
+// });
 
-new Splide(".splide").mount(window.splide.Extensions);
-import Splide from "@splidejs/splide";
-import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
-
-const splide = new Splide(".splide", {
-  type: "loop",
-  drag: "free",
-  focus: "center",
-  perPage: 3,
-  autoScroll: {
-    speed: 1,
+var swiper = new Swiper(".swiper-slide", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
 
-splide.mount();
-new Splide(".splide", {
-  autoScroll: {
-    speed: 2,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide("#splide-autoscroll", {
+    type: "loop",
+    drag: "free",
+    focus: "center",
+    perPage: 3,
+    autoScroll: {
+      speed: 1,
+    },
+  });
+
+  splide.mount(window.splide.Extensions);
 });
