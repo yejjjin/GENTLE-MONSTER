c0a662103766334cb4c6dc4f91df688f7b909069
yejjjin <151498476+yejjjin@users.noreply.github.com>
2024-01-02 21:39:02 +0900

    240102 work2

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -13,6 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
   splide.mount(window.splide.Extensions);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  var splideOptions = {
+    type: "loop",
+    drag: "free",
+    focus: "center",
+    autoScroll: {
+      speed: 1,
+    },
+  };
+
+  // Check screen width and set perPage accordingly
+  if (window.innerWidth >= 1280) {
+    splideOptions.perPage = 0.7;
+  } else {
+    splideOptions.perPage = 1.5;
+  }
+
+  var splide = new Splide("#splide-autoscroll", splideOptions);
+
+  // Additional configuration or methods for the Splide instance can be added here.
+
+  splide.mount(); // Mounts the Splide instance after configuration.
+});
+
 // best seller
 var swiper = new Swiper(".best-slide", {
   slidesPerView: 2,
