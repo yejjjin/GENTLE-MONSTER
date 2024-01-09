b3a5d4d5fd2ecd9ace5ccea57c07ce48148dec2e
yejjjin <151498476+yejjjin@users.noreply.github.com>
2024-01-09 22:53:39 +0900

    240109 WORK2

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -116,3 +116,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// store pc
+window.addEventListener("DOMContentLoaded", fn());
+function fn() {
+  gsap.registerPlugin(ScrollTrigger);
+  const target = document.querySelector('[data-scroll="target"');
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: target,
+      start: "top top",
+      end: "+=150%",
+      scrub: true,
+      pin: true,
+      markers: true,
+      pinSpacing: true,
+    },
+  });
+  tl.fromTo(
+    target,
+    {
+      scale: 0.5,
+    },
+    {
+      scale: 1,
+      ease: "none",
+    }
+  );
+}
